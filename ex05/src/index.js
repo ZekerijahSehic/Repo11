945305bddd977591df168455455997a1304c74a8
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-24 10:30:48 +0200

    Nest one [] within Another []

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this list
+var myNestedArray = [
+    ["Toblerone", 5], 
+    ["Milka", 3]
+];
+
+function myNestedFunction(arr) {
+    return arr;
+}
+// Only change code abov this list
+
+console.log(myNestedFunction(myNestedArray));
+module.exports = myNestedFunction;
